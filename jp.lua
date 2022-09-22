2a2434cdf3fcef6b56d824427eca38cc3120186c
Chris Wilson <chris@legendserver.info>
2022-09-21 23:19:01 -0400

    Lua updates, walkthrough updates

--- jp.lua
+++ jp.lua
@@ -1,5 +1,41 @@
 jp = {}
 
+jp.eggs_remaining_address = 0x000E0F
+jp.eggs = {
+    -- Egg addresses have a value of 0 if collected, 10 otherwise
+    fake_trex_egg = 0x0031C0,
+    south_hill_egg = 0x0031BE,
+    southern_mountain_egg = 0x0031C4,
+    shock_trap_egg = 0x0031BA,
+    deep_jungle_egg = 0x0031BC,
+    west_trike_egg = 0x0031B8,
+    visitor_center_roof_egg = 0x0031B6,
+}
+
+------------------------------------
+--    Park Computer Functions     --
+------------------------------------
+jp.open_gate_1 = function()
+    memory.write_u8(0x00024D, 1) -- Gate 1
+    memory.write_u8(0x00024F, 0) -- Gate 2
+    memory.write_u8(0x000251, 0) -- Gate 3
+end
+
+jp.open_gate_2 = function()
+    memory.write_u8(0x00024D, 0)
+    memory.write_u8(0x00024F, 0)
+    memory.write_u8(0x000251, 0)
+end
+
+jp.open_gate_3 = function()
+    memory.write_u8(0x00024D, 0)
+    memory.write_u8(0x00024F, 0)
+    memory.write_u8(0x000251, 1)
+end
+
+------------------------------------
+--        Cheat Functions         --
+------------------------------------
 jp.grant_ammo = function()
     -- Primary ammo
     memory.write_u16_le(0x00028B, 65535)
@@ -79,13 +115,6 @@ jp.grant_gas = function()
     memory.write_u8(0x00029D, 41)
 end
 
-jp.grant_nublar_utility_shed_south_battery = function()
-    memory.write_u8(0x000269, 255) -- Grants battery to player
-    memory.write_u8(0x00026A, 255) -- Unknown, changes when battery is picked up
-    memory.write_u8(0x001DE8, 255) -- Unknown, changes when battery is picked up
-    memory.write_u8(0x00C635, 255) -- Removes battery from map when value > 127
-end
-
 jp.activate_power_generator = function()
     -- Activate power generator. To disable, set to 0
     memory.write_u16_le(0x00026B, 65535)
@@ -100,4 +129,66 @@ end
 jp.disable_power_generator = function()
     memory.write_u16_le(0x00026B, 0) -- Disable power generator
     memory.write_u8(0x01314D, 212) -- Set power panel graphic to not activated
+end
+
+jp.open_all_gates = function()
+    memory.write_u8(0x00024D, 1)
+    memory.write_u8(0x00024F, 1)
+    memory.write_u8(0x000251, 1)
+end
+
+-----------------------------------
+--     Night vision goggles      --
+-----------------------------------
+jp.grant_battery_temp = function()
+    memory.write_u16_le(0x000269, 65535) -- Grants battery for current building
+end
+
+jp.grant_south_utility_shed_battery = function()
+    memory.write_u8(0x001DE8, 255) -- Determines if the battery has been acquired in this building
+    memory.write_u8(0x00C635, 255) -- Removes battery from map when value > 127
+end
+
+jp.grant_beach_utility_shed_battery = function()
+    memory.write_u8(0x001DE6, 255) -- Determines if the battery has been acquired in this building
+    memory.write_u8(0x00C3A5, 255) -- Removes battery from map when value > 127
+end
+
+-----------------------------------
+--       ID Card Functions       --
+-----------------------------------
+jp.grant_dennis_nedry = function()
+    memory.write_u16_le(0x00025F, 65535) -- Grant ID card to player
+    memory.write_u8(0x00B179, 255) -- Remove ID card from floor
+    memory.write_u8(0x00B17A, 42) -- Remove ID card from floor
+end
+
+jp.revoke_dennis_nedry = function()
+    memory.write_u16_le(0x00025F, 0) -- Remove ID card from player
+    memory.write_u8(0x00B179, 0) -- Place ID card back on the floor
+    memory.write_u8(0x00B17A, 2) -- Place ID card back on the floor
+end
+
+jp.grant_john_hammond = function()
+    memory.write_u16_le(0x008862, 65535) -- Grant ID card to player
+    memory.write_u8(0x000253, 1) -- Remove ID card from floor
+    memory.write_u8(0x003246, 0) -- Remove ID card from floor
+end
+
+jp.revoke_john_hammond = function()
+    memory.write_u16_le(0x008862, 0) -- Remove ID card from player
+    memory.write_u8(0x000253, 0) -- Place ID card back on the floor
+    memory.write_u8(0x003246, 16) -- Place ID card back on the floor
+end
+
+jp.grant_alan_grant = function()
+    memory.write_u16_le(0x000259, 65535) -- Grant ID card to player
+    memory.write_u8(0x00B95B, 255) -- Remove ID card from floor
+    memory.write_u8(0x00B95C, 42) -- Remove ID card from floor
+end
+
+jp.revoke_alan_grant = function()
+    memory.write_u16_le(0x000259, 0) -- Remove ID card from player
+    memory.write_u8(0x00B95B, 0) -- Place ID card back on the floor
+    memory.write_u8(0x00B95C, 2) -- Place ID card back on the floor
 end
--- vanilla-walkthrough.md
+++ vanilla-walkthrough.md
@@ -1,9 +1,30 @@
 # Jurassic Park (SNES) Walkthrough
 
-1. Nublar Utility Shed (South)
-   1. Acquire Battery
-   2. Turn on Power Generator
+1. South Utility Shed
+   1. Acquire Battery (0F)
+   2. Turn on Power Generator (0F)
    
 2. Acquire Eggs
-   1. 
+   1. Fake T-Rex Egg
+   2. South Hill Egg
+   3. Western Mountain Egg
+   4. Shock Trap Egg
+   5. Deep Jungle Egg
+   6. West Trike Egg
+   
+3. Beach Utility Shed
+   1. Acquire Dennis Nedry's ID Card (0F)
 
+4. Visitor's Center
+   1. Reboot Park System (1F)
+   2. Enable Motion Sensors (1F)
+   3. Acquire Visitor's Center Roof Egg (Roof)
+   4. Acquire John Hammond's ID Card (Roof)
+   5. Acquire Battery (B1)
+   6. Acquire Alan Grant's ID Card (0F)
+
+5. Raptor Paddock
+   1. Acquire Battery (0F)
+   2. Acquire Ian Malcolm's ID Card (1F)
+   3. Acquire Robert Muldoon's ID Card (B1)
+   4. Push crate in front of door (B2)
